8a3ad6694a77b9ebe1b7dd1566cc656444d9c215
daystar watson <daystarwatson@gmail.com>
2025-12-15 16:11:53 -0800

    latest commit

--- app.py
+++ app.py
@@ -1,91 +1,94 @@
-def greet_user():
-    print("welcome! good day, dear passenger.")
+
+def calculate(expression):
+    expression = expression.replace(" ", "")
+    items = char_expression(expression)
+    items = solve_inner_brackets(items)
+    return evaluate(items)
 
 
-def pick_ups():
-    nigeria_pick_ups = ["lagos", "rivers",
-                        "abia", "abuja", "kano", "enugu", "imo"]
-    pick_ups = input("please, enter a pick up location: ").lower()
-    if pick_ups not in nigeria_pick_ups:
-        print("sorry, we dont operate outside nigria. try again")
-        pick_ups = input("please, enter a pick up location: ").lower()
-
-    return pick_ups
-
-
-def nigeria_drop_offs():
-    nigeria_drop_offs = ["ikeja", "port_harcourt",
-                         "fct", "calabar", "oron", "asaba"]
-    drop_off = input("please, enter your drop off location: ")
-    if drop_off not in nigeria_drop_offs:
-        print("sorry, drop off location is outside of nigeria. try again")
-        drop_off = input("please, enter your drop off location: ")
-    return drop_off
+def char_expression(expression):
+    num = ""
+    items = []
+    for char in expression:
+        if char.isdigit() or char == ".":
+            num += char
+        else:
+            if num:
+                items.append(float(num))
+                num = ""
+                items.append(char)
+    if num:
+        items.append(float(num))
+    return items
 
 
-def start_ride():
-    start_ride = input("should we start the ride? (yes/no/hold on): ").lower()
+def solve_inner_brackets(items):
+    while "(" in items:
+        open_index = None
+        close_index = None
+        i = 0
+        for x in range(len(items)):
+            if x == "(":
+                open_index = i
+            elif x == ")":
+                close_index = i
 
-    if start_ride == "yes":
-        print("ride started, driving to your destination now")
-
-    elif start_ride == "no":
-        print("ride has not started, waiting for you to start the ride")
-        start_ride = input(
-            "should we start the ride? (yes/no/hold on): ").lower()
-
-    else:
-        start_ride == "hold on"
-        print("okay, say go when you want me to go")
-        start_ride = input(
-            "should we start the ride? (go): ").lower()
-    return start_ride
-
-
-def payments(ride_price):
-    print(f"the price for the ride is ${ride_price}")
-    amount = int(input("enter the amount for the payment of the ride: $"))
-    if amount < ride_price:
-        print("amount is insufficient. please enter the right amount")
-        amount = int(input("enter the amount for the payment of the ride: $"))
-
-    elif amount == ride_price:
-        print("payment successful. exact amount thank you")
-
-    else:
-        amount > ride_price
-        change = amount - ride_price
-        print("payment succesful, your change is:", change)
-    return amount
+        things_inside = items[open_index + 1: close_index]
+        answer = evaluate(things_inside)
+        left_side = items[:open_index]
+        right_side = items[close_index + 1:]
+        items = left_side + [answer] + right_side
+        break
+    return items
 
 
-def ask_for_tips():
-    tip = input(
-        "would you like to give a tip? type 'AMOUNT' or type 'NO': ").lower()
-
-    if tip == "no":
-        print("thank you for riding with us")
-    else:
-        tip = int(tip)
-        print("thank you so much for the tip of $", tip)
-
-        print("have a lovely day")
-        print("good bye")
-
-
-def main_program():
-
-    greet_user()
-
-    pick_ups()
-
-    nigeria_drop_offs()
-
-    start_ride()
-
-    payments(5000)
-
-    ask_for_tips()
+def solve_multi_div(items):
+    i = 0
+    while i < len(items):
+        current = items[i]
+        if current == "*":
+            left = items[i - 1]
+            right = items[i + 1]
+            result = left * right
+            items[i - 1: 1 + 2] = [result]
+            i -= 1
+        elif current == "/":
+            left = items[i - 1]
+            right = items[i + 1]
+            result = left / right
+            items[i - 1:i + 2] = [result]
+            i -= 1
+        else:
+            i += 1
+    return items
 
 
-main_program()
+def solve_add_sub(items):
+    i = 0
+    while i < len(items):
+        current = items[i]
+        if current == "+":
+            left = items[i - 1]
+            right = items[i + 1]
+            result = left + right
+            items[i - 1:i + 2] = [result]
+            i -= 1
+        elif current == "-":
+            left = items[i - 1]
+            right = items[i + 1]
+            result = left - right
+            items[i - 1:i + 2] = [result]
+            i -= 1
+        else:
+            i += 1
+    return items
+
+
+def evaluate(items):
+    items = solve_multi_div(items)
+    items = solve_add_sub(items)
+    return items[0]
+
+
+expression = input("enter your calculatons: ")
+print(calculate(expression))
